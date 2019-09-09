477ea346cca99ac2a894bac80b37febbf31a159f
Tessa <Tessa@DESKTOP-38ODN6B>
2019-09-09 16:13:26 -0400

    Added Entity Framework

---
+++ MyShop/MyShop.DataAccess.SQL/DataContext.cs
@@ -0,0 +1,25 @@
+﻿using MyShop.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyShop.DataAccess.SQL
+{
+    public class DataContext : DbContext
+    {
+        //Contructor to captire and pass in the connection string that base classes expecting
+        public DataContext()
+            //Looks for "DefailtConnection" in the Web.config
+            //Can acturally pass in a connection string
+            : base("DefaultConnection")
+        {
+
+        }
+
+        public DbSet<Product> Products { get; set; }
+        public DbSet<ProductCategory> ProductCategories { get; set; }
+    }
+}
